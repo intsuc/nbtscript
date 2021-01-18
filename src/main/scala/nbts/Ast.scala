e27a3b3348cd54baf6e87b0cbc2918504e62cc4f
intsuc <intsuc@gmail.com>
2021-01-18 17:09:36 +0900

    Add AST

---
+++ src/main/scala/nbts/Ast.scala
@@ -0,0 +1,41 @@
+package nbts
+
+object Ast:
+  enum Tag:
+    case String(data: java.lang.String)
+    case Compound(data: Map[java.lang.String, Tag])
+    case Byte(data: scala.Byte)
+    case Short(data: scala.Short)
+    case Int(data: scala.Int)
+    case Long(data: scala.Long)
+    case Float(data: scala.Float)
+    case Double(data: scala.Double)
+    case ByteArray(data: Seq[scala.Byte])
+    case IntArray(data: Seq[scala.Int])
+    case LongArray(data: Seq[scala.Long])
+    case List(data: Seq[Tag])
+
+  enum Node:
+    case MatchRootObject(pattern: CompoundTag)
+    case MatchObject(name: String, pattern: CompoundTag)
+    case AllElements
+    case MatchElement(pattern: CompoundTag)
+    case IndexedElement(index: Int)
+    case CompoundChild(name: String)
+
+  final case class Path(nodes: Seq[Node])
+
+  final case class Access(tag: Tag, path: Option[Path])
+
+  enum Statement:
+    case Insert(index: Int, target: Access, source: Access)
+    case Prepend(target: Access, source: Access)
+    case Append(target: Access, source: Access)
+    case Set(target: Access, source: Access)
+    case Remove(target: Access)
+    case Get(target: Access)
+    case GetNumeric(target: Access, scale: Double)
+    case Merge(target: Access, source: Access)
+    case Print(target: Access)
+
+  final case class Source(statements: Seq[Statement])
