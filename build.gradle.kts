01a7023754080677d0ec26d54277678e2e89950d
intsuc <intsuc@gmail.com>
2022-08-12 09:09:39 +0900

    Set main class

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.10"
+    application
 }
 
 version = "0.1.0"
@@ -23,4 +24,8 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "18"
+}
+
+application {
+    mainClass.set("nbtscript.cli.MainKt")
 }
