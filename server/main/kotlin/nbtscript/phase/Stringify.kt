623f3efb94ba105e68c31550cee54ac2fc68129e
intsuc <intsuc@gmail.com>
2022-08-14 11:10:59 +0900

    Fix stringify

--- server/main/kotlin/nbtscript/phase/Stringify.kt
+++ server/main/kotlin/nbtscript/phase/Stringify.kt
@@ -17,7 +17,7 @@ fun stringifyTypeZ(type: TypeZ): String = when (type) {
     is TypeZ.IntArrayZ -> "int_array"
     is TypeZ.LongArrayZ -> "long_array"
     is TypeZ.ListZ -> "list ${stringifyTypeZ(type.element)}"
-    is TypeZ.CompoundZ -> type.elements.entries.joinToString("compound {", ", ", "}") { "${it.key}: ${stringifyTypeZ(it.value)}" }
+    is TypeZ.CompoundZ -> type.elements.entries.joinToString(", ", "compound {", "}") { "${it.key}: ${stringifyTypeZ(it.value)}" }
     is TypeZ.Hole -> "hole"
 }
 
@@ -29,11 +29,11 @@ fun stringifyTermZ(term: TermZ): String = when (term) {
     is TermZ.FloatTag -> "${term.data}f"
     is TermZ.DoubleTag -> "${term.data}d"
     is TermZ.StringTag -> "\"${term.data}\"" // TODO: escape
-    is TermZ.ByteArrayTag -> term.elements.joinToString("[B;", ", ", "]") { stringifyTermZ(it) }
-    is TermZ.IntArrayTag -> term.elements.joinToString("[I;", ", ", "]") { stringifyTermZ(it) }
-    is TermZ.LongArrayTag -> term.elements.joinToString("[L;", ", ", "]") { stringifyTermZ(it) }
-    is TermZ.ListTag -> term.elements.joinToString("[", ", ", "]") { stringifyTermZ(it) }
-    is TermZ.CompoundTag -> term.elements.entries.joinToString("{", ", ", "}") { "${it.key}: ${stringifyTermZ(it.value)}" }
+    is TermZ.ByteArrayTag -> term.elements.joinToString(", ", "[B;", "]") { stringifyTermZ(it) }
+    is TermZ.IntArrayTag -> term.elements.joinToString(", ", "[I;", "]") { stringifyTermZ(it) }
+    is TermZ.LongArrayTag -> term.elements.joinToString(", ", "[L;", "]") { stringifyTermZ(it) }
+    is TermZ.ListTag -> term.elements.joinToString(", ", "[", "]") { stringifyTermZ(it) }
+    is TermZ.CompoundTag -> term.elements.entries.joinToString(", ", "{", "}") { "${it.key}: ${stringifyTermZ(it.value)}" }
     is TermZ.Splice -> "$${stringifyTermS(term.element)}"
     is TermZ.Function -> "function ${term.name} = ${stringifyTermZ(term.body)};\n${stringifyTermZ(term.next)}"
     is TermZ.Run -> term.name
@@ -54,7 +54,7 @@ fun stringifyTermS(term: TermS): String = when (term) {
     is TermS.IntArrayS -> "int_array"
     is TermS.LongArrayS -> "long_array"
     is TermS.ListS -> "list ${stringifyTermS(term.element)}"
-    is TermS.CompoundS -> term.elements.entries.joinToString("compound {", ", ", "}") { "${it.key}: ${stringifyTermS(it.value)}" }
+    is TermS.CompoundS -> term.elements.entries.joinToString(", ", "compound {", "}") { "${it.key}: ${stringifyTermS(it.value)}" }
     is TermS.ArrowS -> "arrow ${term.name?.let { "$it: " } ?: ""}${stringifyTermS(term.dom)}. ${stringifyTermS(term.cod)}"
     is TermS.CodeS -> "code ${stringifyTypeZ(term.element)}"
     is TermS.TypeZ -> "type"
@@ -66,11 +66,11 @@ fun stringifyTermS(term: TermS): String = when (term) {
     is TermS.FloatTag -> "${term.data}f"
     is TermS.DoubleTag -> "${term.data}d"
     is TermS.StringTag -> "\"${term.data}\"" // TODO: escape
-    is TermS.ByteArrayTag -> term.elements.joinToString("[B;", ", ", "]") { stringifyTermS(it) }
-    is TermS.IntArrayTag -> term.elements.joinToString("[I;", ", ", "]") { stringifyTermS(it) }
-    is TermS.LongArrayTag -> term.elements.joinToString("[L;", ", ", "]") { stringifyTermS(it) }
-    is TermS.ListTag -> term.elements.joinToString("[", ", ", "]") { stringifyTermS(it) }
-    is TermS.CompoundTag -> term.elements.entries.joinToString("{", ", ", "}") { "${it.key}: ${stringifyTermS(it.value)}" }
+    is TermS.ByteArrayTag -> term.elements.joinToString(", ", "[B;", "]") { stringifyTermS(it) }
+    is TermS.IntArrayTag -> term.elements.joinToString(", ", "[I;", "]") { stringifyTermS(it) }
+    is TermS.LongArrayTag -> term.elements.joinToString(", ", "[L;", "]") { stringifyTermS(it) }
+    is TermS.ListTag -> term.elements.joinToString(", ", "[", "]") { stringifyTermS(it) }
+    is TermS.CompoundTag -> term.elements.entries.joinToString(", ", "{", "}") { "${it.key}: ${stringifyTermS(it.value)}" }
     is TermS.Abs -> "\\${term.name}: ${stringifyTermS(term.anno)}. ${stringifyTermS(term.body)}"
     is TermS.Apply -> "@${stringifyTermS(term.operator)} ${stringifyTermS(term.operand)}"
     is TermS.Quote -> "`${stringifyTermZ(term.element)}"
