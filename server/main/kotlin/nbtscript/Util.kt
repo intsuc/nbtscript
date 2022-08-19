09aae304b320040648a620408710d91a9d0e990f
intsuc <intsuc@gmail.com>
2022-08-20 00:32:37 +0900

    Remove redundant `reified`

--- server/main/kotlin/nbtscript/Util.kt
+++ server/main/kotlin/nbtscript/Util.kt
@@ -1,6 +1,6 @@
 package nbtscript
 
-inline fun <reified A, reified B> cast(a: A): B = a as B
+inline fun <A, reified B> cast(a: A): B = a as B
 
 @Suppress("NOTHING_TO_INLINE")
 inline fun unreachable(): Nothing = error("unreachable")
