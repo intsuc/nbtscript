0b6747c6dc5913b130c91d2c1f1e80f950ab5192
intsuc <intsuc@gmail.com>
2022-08-20 16:40:15 +0900

    Make ranges more precise

--- server/main/kotlin/nbtscript/phase/Parse.kt
+++ server/main/kotlin/nbtscript/phase/Parse.kt
@@ -35,7 +35,7 @@ class Parse private constructor(
         term
     }
 
-    private fun RangeContext.parseTerm1(): Term =
+    private fun parseTerm1(): Term = ranged {
         when (peek()) {
             '(' -> {
                 skip()
@@ -113,8 +113,9 @@ class Parse private constructor(
                 }
             }
         }
+    }
 
-    private fun RangeContext.parseTerm2(): Term =
+    private fun parseTerm2(): Term = ranged outer@{
         when (val start = peek()) {
             '(' -> {
                 skip()
@@ -180,102 +181,106 @@ class Parse private constructor(
 
             null -> hole()
             else -> {
-                val word = readString()
-                if (start.isNumericStart()) {
-                    when {
-                        word.endsWith('b') -> Term.ByteTag(word.dropLast(1).toByte(), range())
-                        word.endsWith('s') -> Term.ShortTag(word.dropLast(1).toShort(), range())
-                        word.endsWith('L') -> Term.LongTag(word.dropLast(1).toLong(), range())
-                        word.endsWith('f') -> Term.FloatTag(word.dropLast(1).toFloat(), range())
-                        word.endsWith('d') -> Term.DoubleTag(word.dropLast(1).toDouble(), range())
-                        else -> {
-                            when (val int = word.toIntOrNull()) {
-                                null -> hole()
-                                else -> Term.IntTag(int, range())
-                            }
-                        }
-                    }
-                } else {
-                    when (word) {
-                        "universe" -> Term.UniverseType(range())
-                        "end" -> Term.EndType(range())
-                        "byte" -> Term.ByteType(range())
-                        "short" -> Term.ShortType(range())
-                        "int" -> Term.IntType(range())
-                        "long" -> Term.LongType(range())
-                        "float" -> Term.FloatType(range())
-                        "double" -> Term.DoubleType(range())
-                        "string" -> Term.StringType(range())
-                        "collection" -> {
-                            val element = parseTerm2()
-                            Term.CollectionType(element, range())
-                        }
-
-                        "byte_array" -> Term.ByteArrayType(range())
-                        "int_array" -> Term.IntArrayType(range())
-                        "long_array" -> Term.LongArrayType(range())
-                        "list" -> {
-                            val element = parseTerm2()
-                            Term.ListType(element, range())
-                        }
-
-                        "compound" -> {
-                            expect('{')
-                            val elements = parseList('}') {
-                                val key = parseName()
-                                expect(':')
-                                val value = parseTerm()
-                                key to value
-                            }.toMap()
-                            Term.CompoundType(elements, range())
-                        }
-
-                        "code" -> {
-                            val element = parseTerm2()
-                            Term.CodeType(element, range())
-                        }
-
-                        "type" -> Term.TypeType(range())
-                        "let" -> {
-                            val name = parseName()
-                            val anno = when (peek()) {
-                                ':' -> {
-                                    skip()
-                                    parseTerm()
+                skipWhitespace()
+                ranged {
+                    val word = readString()
+                    if (start.isNumericStart()) {
+                        when {
+                            word.endsWith('b') -> Term.ByteTag(word.dropLast(1).toByte(), range())
+                            word.endsWith('s') -> Term.ShortTag(word.dropLast(1).toShort(), range())
+                            word.endsWith('L') -> Term.LongTag(word.dropLast(1).toLong(), range())
+                            word.endsWith('f') -> Term.FloatTag(word.dropLast(1).toFloat(), range())
+                            word.endsWith('d') -> Term.DoubleTag(word.dropLast(1).toDouble(), range())
+                            else -> {
+                                when (val int = word.toIntOrNull()) {
+                                    null -> this@outer.hole()
+                                    else -> Term.IntTag(int, range())
                                 }
-
-                                else -> null
                             }
-                            expect('=')
-                            val init = parseTerm()
-                            expect(';')
-                            val next = parseTerm()
-                            Term.Let(name, anno, init, next, range())
                         }
+                    } else {
+                        when (word) {
+                            "universe" -> Term.UniverseType(range())
+                            "end" -> Term.EndType(range())
+                            "byte" -> Term.ByteType(range())
+                            "short" -> Term.ShortType(range())
+                            "int" -> Term.IntType(range())
+                            "long" -> Term.LongType(range())
+                            "float" -> Term.FloatType(range())
+                            "double" -> Term.DoubleType(range())
+                            "string" -> Term.StringType(range())
+                            "collection" -> {
+                                val element = parseTerm2()
+                                Term.CollectionType(element, range())
+                            }
 
-                        "fun" -> {
-                            val name = parseName()
-                            val anno = when (peek()) {
-                                ':' -> {
-                                    skip()
-                                    parseTerm()
+                            "byte_array" -> Term.ByteArrayType(range())
+                            "int_array" -> Term.IntArrayType(range())
+                            "long_array" -> Term.LongArrayType(range())
+                            "list" -> {
+                                val element = parseTerm2()
+                                Term.ListType(element, range())
+                            }
+
+                            "compound" -> {
+                                expect('{')
+                                val elements = parseList('}') {
+                                    val key = parseName()
+                                    expect(':')
+                                    val value = parseTerm()
+                                    key to value
+                                }.toMap()
+                                Term.CompoundType(elements, range())
+                            }
+
+                            "code" -> {
+                                val element = parseTerm2()
+                                Term.CodeType(element, range())
+                            }
+
+                            "type" -> Term.TypeType(range())
+                            "let" -> {
+                                val name = parseName()
+                                val anno = when (peek()) {
+                                    ':' -> {
+                                        skip()
+                                        parseTerm()
+                                    }
+
+                                    else -> null
                                 }
-
-                                else -> null
+                                expect('=')
+                                val init = parseTerm()
+                                expect(';')
+                                val next = parseTerm()
+                                Term.Let(name, anno, init, next, range())
                             }
-                            expect('=')
-                            val body = parseTerm()
-                            expect(';')
-                            val next = parseTerm()
-                            Term.Fun(name, anno, body, next, range())
-                        }
 
-                        "" -> hole()
-                        else -> Term.Var(word, range())
+                            "fun" -> {
+                                val name = parseName()
+                                val anno = when (peek()) {
+                                    ':' -> {
+                                        skip()
+                                        parseTerm()
+                                    }
+
+                                    else -> null
+                                }
+                                expect('=')
+                                val body = parseTerm()
+                                expect(';')
+                                val next = parseTerm()
+                                Term.Fun(name, anno, body, next, range())
+                            }
+
+                            "" -> this@outer.hole()
+                            else -> Term.Var(word, range())
+                        }
                     }
                 }
             }
         }
+    }
 
     // TODO: improve error reporting and locality
     private fun parseStringTag(): Term = ranged {
@@ -324,15 +329,17 @@ class Parse private constructor(
         return elements
     }
 
-    private fun parseName(): Name = ranged {
-        val text = readString()
-        val range = range()
-        if (text.isEmpty()) context.addDiagnostic(wordExpected(range))
-        Name(text, range)
+    private fun parseName(): Name {
+        skipWhitespace()
+        return ranged {
+            val text = readString()
+            val range = range()
+            if (text.isEmpty()) context.addDiagnostic(wordExpected(range))
+            Name(text, range)
+        }
     }
 
     private fun readString(): String {
-        skipWhitespace()
         val start = cursor
         while (text.getOrNull(cursor)?.isWordPart() == true) skip()
         return text.substring(start, cursor)
